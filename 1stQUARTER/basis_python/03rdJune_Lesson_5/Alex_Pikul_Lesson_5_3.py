17fa0eeca7bb5a4067a1839bf836a9df2fd35bee
ALEX <mmm@mmm.com>
2022-06-05 10:53:40 +0200

    Alex_Pikul_Lesson_5_3 completed

--- 1stQUARTER/basis_python/03rdJune_Lesson_5/Alex_Pikul_Lesson_5_3.py
+++ 1stQUARTER/basis_python/03rdJune_Lesson_5/Alex_Pikul_Lesson_5_3.py
@@ -20,11 +20,29 @@ klasses = [
 """
 
 
+def make_gen_from_2lst(lst_1, lst_2):
+    """Функция принимает 2 листа и возвращает картежи вида <lst_1>, <lst_2>"""
+    i = 0
+    while i < len(lst_1) and i < len(lst_2):
+        yield lst_1[i], lst_2[i]
+        i += 1
+    while i < len(lst_1):
+        yield lst_1[i], None
+        i += 1
 
 
+tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена', 'Иван', 'Данила']
+klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
 
+#   проверяем случай (<tutor>, None)
+for i in make_gen_from_2lst(tutors, klasses):
+    print(i, type(i))
 
+#   присваиваем генератор переменной и вызываем до истощения
+lst_gen = make_gen_from_2lst(tutors, klasses)
+# print(lst_gen, next(lst_gen), next(lst_gen), next(lst_gen), next(lst_gen), next(lst_gen), next(lst_gen),
+#       next(lst_gen), next(lst_gen), next(lst_gen), next(lst_gen), sep='\n')
 
-
-
-
+#   или
+for i in range(10):
+    print(next(lst_gen))
