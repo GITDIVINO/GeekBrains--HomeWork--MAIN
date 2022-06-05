4e187b4ca957f1b4d27fdc7ff896792682552dc6
ALEX <mmm@mmm.com>
2022-06-05 23:05:01 +0200

    Alex_Pikul_Lesson_5_4 completed

--- 1stQUARTER/basis_python/03rdJune_Lesson_5/Alex_Pikul_Lesson_5_4.py
+++ 1stQUARTER/basis_python/03rdJune_Lesson_5/Alex_Pikul_Lesson_5_4.py
@@ -5,12 +5,18 @@ result = [12, 44, 4, 10, 78, 123]
 Подсказка: использовать возможности python, изученные на уроке.
 """
 
+src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 
 
+def make_new_lst(any_lst):
+    amm_lst = list(zip(any_lst[1:], any_lst))
+    result_lst = []
+
+    for element in amm_lst:
+        if element[1] < element[0]:
+            result_lst.append(element[0])
+
+    return result_lst
 
 
-
-
-
-
-
+print(make_new_lst(src))
