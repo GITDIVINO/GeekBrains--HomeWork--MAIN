57de07a1750559c675972fa98799d9d150f4adba
ALEX <mmm@mmm.com>
2022-06-07 14:19:38 +0200

    Lesson_6_ex_2 completed

--- 1stQUARTER/basis_python/07thJune_Lesson_6/Alex_Pikul_Lesson_6_ex_2/Alex_Pikul_Lesson_6_ex_2.py
+++ 1stQUARTER/basis_python/07thJune_Lesson_6/Alex_Pikul_Lesson_6_ex_2/Alex_Pikul_Lesson_6_ex_2.py
@@ -5,31 +5,32 @@
 даже с файлами, размер которых превышает объем ОЗУ компьютера.
 """
 
-file_1 = open('Alex_Pikul_Lesson_6_ex_1/nginx_logs.txt', 'r', encoding='utf-8')
+file_1 = open(r'C:\Users\User\Desktop\GeekBrains\GeekBrains__HomeWork__MAIN\1stQUARTER\basis_python\07thJune_Lesson_6'
+              r'\Alex_Pikul_Lesson_6_ex_1\nginx_logs.txt', 'r', encoding='utf-8')
 
 
 def fnd_spammer(any_log_file):
-    ip_lst = []
+    """Функция принимает файл с логами сайта"""
+    with open('ip_list.txt', 'w+', encoding='utf-8') as f:  # отделяем ip адреса и записываем в файл ip_list.txt
+        for line in any_log_file:
+            f.writelines(f'{line.split(" ")[0]}\n')
 
-    for ip in any_log_file:
-        ip_lst.append(any_log_file.readline().split(' ')[0])
+    # открываем файл ip_list.txt
+    file_unc_ip = open(r'C:\Users\User\Desktop\GeekBrains\GeekBrains__HomeWork__MAIN\1stQUARTER\basis_python'
+                       r'\07thJune_Lesson_6\Alex_Pikul_Lesson_6_ex_2\ip_list.txt', 'r', encoding='utf-8')
 
-    unic_ip = set(ip_lst)
+    ip_lst = [line.strip() for line in file_unc_ip]     # заносим ip адреса в список
 
-    spammer = {}
-    for ip in unic_ip:
-        a = ip_lst.count(ip)
-        spammer[ip] = a
+    # проходим по списку и ловим спамера
+    max_quan = 1
+    spammer_ip = None
+    for ip in ip_lst:
+        req_quan = ip_lst.count(ip)
+        if req_quan > max_quan:
+            max_quan = req_quan
+            spammer_ip = ip
 
-    return spammer
-
-
-print(fnd_spammer(file_1))
-
-
-
-
+    return spammer_ip, max_quan
 
 
-
-
+print(f'Спамер: {fnd_spammer(file_1)} запросов')
