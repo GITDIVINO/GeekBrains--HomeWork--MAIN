4b8e98a246ac7c025b65d20b4f113f0f3967c385
ALEX <mmm@mmm.com>
2022-06-25 18:02:25 +0200

    I don`t know what to write here

--- FREE_FLOATING/WAGENBORG SHIPPING/FREIGHT_BOT_2.py
+++ FREE_FLOATING/WAGENBORG SHIPPING/FREIGHT_BOT_2.py
@@ -6,9 +6,11 @@ from vessels.make_vsl_list import vessels_lst
 from keyboards.user_kb import kb_user
 from aiogram.types import ReplyKeyboardRemove
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram import types
 
 storage = MemoryStorage()
-
 bot = Bot(token='5485308566:AAEIeTY0SqaR5XKo70zY6QMcEBJixDmG74Q')
 dp = Dispatcher(bot, storage=storage)
 
@@ -16,33 +18,52 @@ dp = Dispatcher(bot, storage=storage)
 # greetings
 @dp.message_handler(commands=['start', 'help'])
 async def command_start(massage: types.Message):
-    await bot.send_message(massage.from_user.id, 'The FREIGTH_BOT will help you to calculate voyage, bunker '
-                                                 'consumption and intake with Wagenborg Shipping. Please choose name '
-                                                 'of the vessel', reply_markup=kb_user)
+    await bot.send_message(massage.from_user.id, 'The FREIGHT_BOT will help you to calculate voyage, bunker '
+                                                 'consumption and intake', reply_markup=kb_user)
 
 
-PORT_OF_LOADING = ''
-DIST_TO_DISPORT = ''
-VESSEL_NAME = ''
-VESSEL_SPEED = ''
-VESSEL_CONSUMPTION = ''
-VESSEL_CAPACITY = ''
-DATE_OF_BL = ''
-MGO_ROTTERDAM = ''
-RATE_OF_EXCHANGE = ''
+class FSMAdmin(StatesGroup):
+    PORT_OF_LOADING = State()
+    DIST_TO_DISPORT = State()
+    VESSEL_NAME = State()
+    VESSEL_SPEED = State()
+    VESSEL_CONSUMPTION = State()
+    VESSEL_CAPACITY = State()
+    DATE_OF_BL = State()
+    MGO_ROTTERDAM = State()
+    RATE_OF_EXCHANGE = State()
+
+
+# WAGENBORG SHIPPING
+@dp.message_handler(commands='WAGENBORG', state=None)
+async def command_start(message: types.Message):
+    await FSMAdmin.VESSEL_NAME.set()
+    await message.reply('What vessel performed the shipment?')
 
 
 # take info from vessel`s description
-@dp.message_handler(commands=vessels_lst)
-async def command_start(message: types.Message):
+@dp.message_handler(commands=vessels_lst, state=FSMAdmin.VESSEL_NAME)
+async def command_start(message: types.Message, state: FSMContext):
+
+   ''' async with state.proxy() as data:
+        data[vessels_lst] = message.
+
     with open(r'C:\Users\User\Desktop\GeekBrains\GeekBrains__HomeWork__MAIN\FREE_FLOATING\WAGENBORG '
               r'SHIPPING\vessels\data.json', 'r', encoding='utf-8') as f:
-        data = json.load(f)
-        VESSEL_NAME = message.text[1:]
-        VESSEL_CONSUMPTION = data[message.text[1:]]['consumption']
-        VESSEL_SPEED = data[message.text[1:]]['speed']
-        VESSEL_CAPACITY = data[message.text[1:]]['consumption']
-    await bot.send_message(message.from_user.id, data[message.text[1:]], reply_markup=ReplyKeyboardRemove())
+        data = json.load(f) '''
+
+    await FSMAdmin.next()
+    await message.reply('Please advise date of BL')
+        
+
+
+
+
+        # VESSEL_CONSUMPTION = data[message.text[1:]]['consumption']
+        # VESSEL_SPEED = data[message.text[1:]]['speed']
+        # VESSEL_CAPACITY = data[message.text[1:]]['consumption']
+    # await bot.send_message(message.from_user.id, data[message.text[1:]], reply_markup=ReplyKeyboardRemove())
+
 
 
 # def bunker_compensation_calc():
--- FREE_FLOATING/WAGENBORG SHIPPING/keyboards/user_kb.py
+++ FREE_FLOATING/WAGENBORG SHIPPING/keyboards/user_kb.py
@@ -1,11 +1,12 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-b1 = KeyboardButton('/adamas')
+b1 = KeyboardButton('WAGENBORG')
+# b1 = KeyboardButton('/adamas')
 b2 = KeyboardButton('/ankie')
 
 kb_user = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 
-kb_user.row(b1, b2)
+kb_user.row(b1)
 
 
 
