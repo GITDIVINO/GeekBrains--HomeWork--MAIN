eddda473afb6f2b1f44114158d9fda0b68a9ba00
ALEX <mmm@mmm.com>
2022-06-29 21:52:08 +0200

    Alex_Pikul_Lesson_10_ex_2 completed

--- 1stQUARTER/basis_python/18thMay_Lesson_10/Alex_Pikul_Lesson_10_ex_2.py
+++ 1stQUARTER/basis_python/18thMay_Lesson_10/Alex_Pikul_Lesson_10_ex_2.py
@@ -9,16 +9,52 @@
 декоратора @property.
 """
 
+from abc import ABC
+from abc import abstractmethod
 
 
+class clothes(ABC):
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod  # abstract method, should be redefined
+    def how_much(self):
+        pass
 
 
+class coat(clothes):
+    def __init__(self, name, v):
+        super().__init__(name)
+        self.v = v
+
+    @property
+    def calc_cunsumption(self):
+        return f'{self.v // 6.5 + 0.5} m fabric spent on {self.name} coat'
+
+    def how_much(self):
+        print('It`s very expensive')
 
 
+class suit(clothes):
+    def __init__(self, name, h):
+        super().__init__(name)
+        self.h = h
+
+    @property
+    def calc_consumption(self):
+        return f'{2 * self.h + 0.3} m fabric spent on {self.name} suit'
+
+    def how_much(self):
+        print('It`s very cheap')
 
 
+if __name__ == '__main__':
+    coat_1 = coat('GUCHI', 43)
+    # print(coat_1.calc_consumption())    # without @property - 6.5 m fabric spent on GUCHI coat
+    print(coat_1.calc_cunsumption)  # @property is working - 6.5 m fabric spent on GUCHI coat
+    coat_1.how_much()  # abstract method
 
-
-
-
-
+    suit_1 = suit('BOSS', 180)
+    # print(suit_1.calc_consumption())    # without @property - 360.3 m fabric spent on BOSS suit
+    print(suit_1.calc_consumption)  # @property is working - 360.3 m fabric spent on BOSS suit
+    suit_1.how_much()  # abstract method
