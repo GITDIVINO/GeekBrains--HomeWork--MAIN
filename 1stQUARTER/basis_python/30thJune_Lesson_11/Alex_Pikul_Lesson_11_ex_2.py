d4203a4af1d007f6a55200edf4eceb5412bfa5d5
ALEX <mmm@mmm.com>
2022-06-30 17:17:21 +0200

    Alex_Pikul_Lesson_11_ex_2 completed

--- 1stQUARTER/basis_python/30thJune_Lesson_11/Alex_Pikul_Lesson_11_ex_2.py
+++ 1stQUARTER/basis_python/30thJune_Lesson_11/Alex_Pikul_Lesson_11_ex_2.py
@@ -1,21 +1,25 @@
 """
-Создать собственный класс-исключение, обрабатывающий ситуацию деления на ноль. Проверить его работу на данных, вводимых
+2. Создать собственный класс-исключение, обрабатывающий ситуацию деления на ноль. Проверить его работу на данных, вводимых
 пользователем. При вводе нуля в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с
 ошибкой.
 """
 
 
+class Myown_ZeroDivisionError(Exception):
+    def __init__(self, first_num, second_num):
+        self.first_num = first_num
+        self.second_num = second_num
 
 
+if __name__ == '__main__':
+    first_num = int(input('Please enter first number: '))
+    second_num = int(input('Please enter second number: '))
 
-
-
-
-
-
-
-
-
-
-
-
+    try:
+        print(first_num / second_num)
+        if second_num == 0:
+            raise Myown_ZeroDivisionError('Myown_ZeroDivisionError: division by zero')
+    except Myown_ZeroDivisionError as err:
+        print(err)
+    else:
+        print(f'All is ok. {first_num} / {second_num} = {first_num/second_num}')
