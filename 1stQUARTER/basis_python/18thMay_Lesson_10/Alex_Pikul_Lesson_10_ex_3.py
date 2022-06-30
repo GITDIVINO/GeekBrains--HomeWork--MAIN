44f780e5c3c96afcd4bfd96da0ac262c02eb413f
ALEX <mmm@mmm.com>
2022-06-30 06:22:17 +0200

    Alex_Pikul_Lesson_10_ex_3 completed

--- 1stQUARTER/basis_python/18thMay_Lesson_10/Alex_Pikul_Lesson_10_ex_3.py
+++ 1stQUARTER/basis_python/18thMay_Lesson_10/Alex_Pikul_Lesson_10_ex_3.py
@@ -8,8 +8,9 @@
 Вычитание. Участвуют две клетки. Операцию необходимо выполнять, только если разность количества ячеек двух клеток больше
 нуля, иначе выводить соответствующее сообщение.
 Умножение. Создаётся общая клетка из двух. Число ячеек общей клетки — произведение количества ячеек этих двух клеток.
-Деление. Создаётся общая клетка из двух.
-Число ячеек общей клетки определяется как целочисленное деление количества ячеек этих двух клеток.
+Деление. Создаётся общая клетка из двух. Число ячеек общей клетки определяется как целочисленное деление количества
+ячеек этих двух клеток.
+
 В классе необходимо реализовать метод make_order(), принимающий экземпляр класса и количество ячеек в ряду. Этот метод
 позволяет организовать ячейки по рядам. Метод должен возвращать строку вида *****\n*****\n*****..., где количество
 ячеек между \n равно переданному аргументу. Если ячеек на формирование ряда не хватает, то в последний ряд записываются
@@ -20,16 +21,56 @@ make_order() вернёт строку: *****\n*****\n*****.
 """
 
 
+class Cell:
+    def __init__(self, nucleus_num):
+        self.nucleus_num = nucleus_num
+
+    def __add__(self, other):
+        cell_3 = Cell(self.nucleus_num + other.nucleus_num)
+        return cell_3
+
+    def __str__(self):
+        return f'{self.nucleus_num} cells'
+
+    def __sub__(self, other):
+        if self.nucleus_num >= other.nucleus_num:
+            return Cell(self.nucleus_num - other.nucleus_num)
+        else:
+            return 'residual of cells is less 0'
+
+    def __mul__(self, other):
+        cell_3 = Cell(self.nucleus_num * other.nucleus_num)
+        return cell_3
+
+    def __floordiv__(self, other):
+        cell_3 = Cell(self.nucleus_num // other.nucleus_num)
+        return cell_3
+
+    def __truediv__(self, other):
+        cell_3 = Cell(self.nucleus_num / other.nucleus_num)
+        return cell_3
+
+    def make_order(self, cells_num):
+        rows_num = self.nucleus_num // cells_num
+        last_row_num = self.nucleus_num % cells_num
+
+        return ('*' * cells_num + '\n') * rows_num + '*' * last_row_num,
 
 
+if __name__ == '__main__':
+    cell_1 = Cell(2500)
+    cell_2 = Cell(17)
 
+    print(cell_1 + cell_2)  # addition
 
+    print(cell_1 - cell_2)  # deduction
+    print(cell_2 - cell_1)
 
+    print(cell_1 * cell_2)  # multiplication
 
+    print(cell_1 // cell_2)  # division
 
+    print(cell_1 / cell_2)  # division
 
-
-
-
-
+    print(cell_2.make_order(5))
 
