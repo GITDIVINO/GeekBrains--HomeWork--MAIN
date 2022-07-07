992bdd538d42bd9fae9d4f1f1ff685f1f4d2b9b0
ALEX <mmm@mmm.com>
2022-07-07 09:05:41 +0200

    2nd quater started, Linux ubuntu installed

--- FREE_FLOATING/FREIGHT_BOT/keyboards/user_kb.py
+++ FREE_FLOATING/FREIGHT_BOT/keyboards/user_kb.py
@@ -1,10 +1,12 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 
-b1 = KeyboardButton('dfhgdfh')
-b2 = KeyboardButton('')
+b1 = KeyboardButton('Voyage calculator')
+b2 = KeyboardButton('Vessel`s description')
+b3 = KeyboardButton('EUR-USD rate of exchange')
+b4 = KeyboardButton('Intake calculator')
 
 kb_user = ReplyKeyboardMarkup()
-kb_user.add(b1)
+kb_user.add(b1).add(b2).add(b3).add(b4)
 
 
 
