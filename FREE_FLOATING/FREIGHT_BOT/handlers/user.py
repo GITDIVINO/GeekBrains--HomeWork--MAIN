ef04fa2b3dbeaef4a5d7191bebfe5f6484519b5a
ALEX <mmm@mmm.com>
2022-07-07 17:51:31 +0200

    added new handlers, for now all working good!

--- FREE_FLOATING/FREIGHT_BOT/handlers/user.py
+++ FREE_FLOATING/FREIGHT_BOT/handlers/user.py
@@ -2,9 +2,8 @@ from aiogram import types, Dispatcher
 from datetime import datetime
 import json
 from create_bot import dp, write_logs
-from keyboards import kb_user, kb_vessels, kb_load_ports
+from keyboards import kb_user, kb_vessels, kb_load_ports, kb_dis_ports, kb_months
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from vessels.make_vsl_list import vessels_lst
 from aiogram.dispatcher import FSMContext
 
 
@@ -31,10 +30,13 @@ async def show_vessels_lst(message: types.Message):
 class VoyageCalculator(StatesGroup):
     VESSEL_NAME = State()
     PORT_OF_LOADING = State()
-    DIST_TO_DISPORT = State()
+    PORT_TO_DISCHARGING = State()
     DATE_OF_BL = State()
+    SHIPMENT_MONTH = State()
+    QUANTITY_AS_PER_BL = State()
 
 
+# start state store
 @dp.message_handler(commands='/Voyage_calculator', state=None)
 async def command_start(message: types.Message):
     await VoyageCalculator.VESSEL_NAME.set()
@@ -42,17 +44,78 @@ async def command_start(message: types.Message):
     write_logs(f'\n{datetime.now()} -- Human (USER ID: {message.from_user.id}) is asking to calculate voyage -->')
 
 
+# catch vessel`s name and request loading port
 @dp.message_handler(state=VoyageCalculator.VESSEL_NAME)
-async def load_vsl_name(message: types.Message, state: FSMContext):
+async def catch_vsl_name(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['VESSEL_NAME'] = message.text
     await VoyageCalculator.next()
-    await message.answer('Please advise Port of loading?', reply_markup=kb_load_ports)
-    write_logs(f'\n{datetime.now()} -- Human (USER ID: {message.from_user.id}) said the vessel` '
-               f'name is {message.text.capitalize()} -->')
+    await message.answer('Please advise Port of Loading?', reply_markup=kb_load_ports)
+    write_logs(f"\n{datetime.now()} -- Human (USER ID: {message.from_user.id}) said the vessel`s "
+               f"name is {data['VESSEL_NAME'].capitalize()} -->")
+
+
+# catch loading port and request discharging port
+@dp.message_handler(state=VoyageCalculator.PORT_OF_LOADING)
+async def catch_loading_port(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['PORT_OF_LOADING'] = message.text
+    await VoyageCalculator.next()
+    await message.answer('Please advise Port of discharging?', reply_markup=kb_dis_ports)
+    write_logs(f"\n{datetime.now()} -- Human (USER ID: {message.from_user.id}) said Port of Loading"
+               f" is {data['PORT_OF_LOADING'].capitalize()} -->")
+
+
+# catch discharging port and request date of B/L
+@dp.message_handler(state=VoyageCalculator.PORT_TO_DISCHARGING)
+async def catch_discharging_port(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['PORT_TO_DISCHARGING'] = message.text
+    await VoyageCalculator.next()
+    await message.answer('Please advise date of B/L in format "yyyy-mm-dd"', reply_markup=types.ReplyKeyboardRemove())
+    write_logs(f"\n{datetime.now()} -- Human (USER ID: {message.from_user.id}) said Port of Discharging"
+               f" is {data['PORT_TO_DISCHARGING'].capitalize()} -->")
+
+
+# catch date of B/L and request shipment`s month
+@dp.message_handler(state=VoyageCalculator.DATE_OF_BL)
+async def catch_bl_date(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['DATE_OF_BL'] = message.text
+    await VoyageCalculator.next()
+    await message.answer('What month of this shipment?', reply_markup=kb_months)
+    write_logs(f"\n{datetime.now()} -- Human (USER ID: {message.from_user.id}) said cargo was loaded"
+               f" on {data['DATE_OF_BL'].capitalize()} -->")
+
+
+# catch shipment`s month and request cargo quantity loaded
+@dp.message_handler(state=VoyageCalculator.SHIPMENT_MONTH)
+async def catch_shipment_month(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['SHIPMENT_MONTH'] = message.text
+    await VoyageCalculator.next()
+    await message.answer('What quantity loaded as per cargo manifest?', reply_markup=types.ReplyKeyboardRemove())
+    write_logs(f"\n{datetime.now()} -- Human (USER ID: {message.from_user.id}) said this"
+               f" is {data['SHIPMENT_MONTH'].capitalize()} shipment -->")
+
+
+# catch cargo quantity loaded
+@dp.message_handler(state=VoyageCalculator.QUANTITY_AS_PER_BL)
+async def catch_quantity_loaded(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['QUANTITY_AS_PER_BL'] = message.text
+    await VoyageCalculator.next()
+    await message.answer('FINISH')
+    write_logs(f"\n{datetime.now()} -- Human (USER ID: {message.from_user.id}) said quantity of cargo loaded"
+               f" is {data['QUANTITY_AS_PER_BL'].capitalize()} -->")
 
 
 def register_handlers_user(dp: Dispatcher):
     dp.register_message_handler(greetings, commands=['start', 'help'])
     dp.register_message_handler(show_vessels_lst, commands=['vessels_list'])
     dp.register_message_handler(command_start, commands=['Voyage_calculator'])
+    dp.register_message_handler(catch_vsl_name)
+    dp.register_message_handler(catch_loading_port)
+    dp.register_message_handler(catch_bl_date)
+    dp.register_message_handler(catch_shipment_month)
+    dp.register_message_handler(catch_quantity_loaded)
--- FREE_FLOATING/FREIGHT_BOT/keyboards/user_kb.py
+++ FREE_FLOATING/FREIGHT_BOT/keyboards/user_kb.py
@@ -7,7 +7,7 @@ b2 = KeyboardButton('/vessels_list')
 b3 = KeyboardButton('EUR-USD rate of exchange')
 b4 = KeyboardButton('Intake calculator')
 
-kb_user = ReplyKeyboardMarkup()
+kb_user = ReplyKeyboardMarkup(resize_keyboard=True)
 kb_user.add(b1).add(b2).add(b3).add(b4)
 
 # vessels buttons
@@ -49,19 +49,54 @@ v35 = KeyboardButton('jade')
 v36 = KeyboardButton('priscilla')
 v37 = KeyboardButton('zeeland')
 
-kb_vessels = ReplyKeyboardMarkup()
+kb_vessels = ReplyKeyboardMarkup(resize_keyboard=True)
 kb_vessels.add(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10,
                v11, v12, v13, v14, v15, v16, v17, v18,
                v19, v20, v21, v22, v23, v24, v25, v26,
                v27, v28, v29, v30, v31, v32, v33, v34,
                v35, v36, v37)
 
+
 # load ports buttons
 lp1 = KeyboardButton('Kaliningrad')
 lp2 = KeyboardButton('Klaipeda')
 
-kb_load_ports = ReplyKeyboardMarkup()
-kb_user.add(lp1).add(lp2)
+kb_load_ports = ReplyKeyboardMarkup(resize_keyboard=True)
+kb_load_ports.add(lp1).add(lp2)
 
 
+# dis ports buttons
+dp1 = KeyboardButton('St. Malo')
+dp2 = KeyboardButton('Le Legue')
+dp3 = KeyboardButton('Rotterdam')
+dp4 = KeyboardButton('Brake')
+dp5 = KeyboardButton('Bremen')
+dp6 = KeyboardButton('WC UK')
+dp7 = KeyboardButton('EC Ireland')
+dp8 = KeyboardButton('Montrose')
+dp9 = KeyboardButton('New Holland')
+dp10 = KeyboardButton('Plymouth')
+dp11 = KeyboardButton('Teignmouth')
+dp12 = KeyboardButton('Brest')
+dp13 = KeyboardButton('Brugge')
 
+kb_dis_ports = ReplyKeyboardMarkup(resize_keyboard=True)
+kb_dis_ports.add(dp1, dp2, dp3, dp4, dp5, dp6, dp7, dp8, dp9, dp10, dp11, dp12, dp13)
+
+
+# dis ports buttons
+m1 = KeyboardButton('January')
+m2 = KeyboardButton('February')
+m3 = KeyboardButton('March')
+m4 = KeyboardButton('April')
+m5 = KeyboardButton('May')
+m6 = KeyboardButton('June')
+m7 = KeyboardButton('July')
+m8 = KeyboardButton('August')
+m9 = KeyboardButton('September')
+m10 = KeyboardButton('November')
+m11 = KeyboardButton('October')
+m12 = KeyboardButton('December')
+
+kb_months = ReplyKeyboardMarkup(resize_keyboard=True)
+kb_months.add(m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12)
